d4f29c3bd19a0c7ee66ee3aeabc05c3b9ac4a289
Mauricio <mauricio.cds00@gmail.com>
2023-02-04 21:20:04 -0300

    feat: event listener

--- src/lists/listeners/create-list-in-crm.listener.ts
+++ src/lists/listeners/create-list-in-crm.listener.ts
@@ -1,1 +1,12 @@
-export class CreateListInCRMListener {}
+import { Injectable } from '@nestjs/common';
+import { ListCreatedEvent } from '../events/list-created.event';
+import { ListGatewayInterface } from '../gateways/list-gateway-interface';
+
+@Injectable()
+export class CreateListInCRMListener {
+  constructor(private listIntegrationGateway: ListGatewayInterface) {}
+
+  public async handle(event: ListCreatedEvent) {
+    this.listIntegrationGateway.create(event.list);
+  }
+}
